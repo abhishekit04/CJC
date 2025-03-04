f87466c6335050452e0801fcf94a062c1b657d13
Abhishek  Singh <88192065+abhishekit04@users.noreply.github.com>
2025-03-05 00:24:38 +0530

    Add files via upload

---
+++ simpleinterest.java
@@ -0,0 +1,16 @@
+
+public class simpleinterest {
+    public static void main(String[] args)
+    {
+        int principle = 2000;
+        int rate = 5;
+        int time = 2;
+
+        int simpleinterest = (principle * rate * time) / 100;
+
+        System.out.println("simpleinterest: " + simpleinterest);
+                        
+    }
+
+
+}
