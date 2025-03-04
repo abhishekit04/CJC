875883acc586538aa168893884c48ef324e9c75e
Abhishek  Singh <88192065+abhishekit04@users.noreply.github.com>
2025-03-05 00:25:03 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic    // class Declaration
+{
+    public static void main(String[] args)
+    {
+        System.out.println(10+10);
+        System.out.println(10-5);
+        System.out.println(2*5);
+        System.out.println(10/2);
+    }
+
+}
